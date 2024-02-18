42d82e78f9649d6164a0be28a0e85153e5f2ffce
Miguel Angel <miguelangelguaivi@gmail.com>
2024-02-18 10:27:18 +0100

    feat: create chat
    The basic functionalities of the chat are finished

--- assets/scripts/chat.js
+++ assets/scripts/chat.js
@@ -5,7 +5,14 @@ let linkedinOption = document.getElementById('linkedinOption');
 let mailOption = document.getElementById('mailOption');
 let cvOption = document.getElementById('cvOption');
 let projectsOption = document.getElementById('projectsOption');
-let chatContent = document.getElementById('chatContent');
+let userChatContent = document.getElementById('userChatContent');
+
+let buttonOptions = [
+    linkedinOption,
+    mailOption,
+    cvOption,
+    projectsOption,
+]
 
 // Open the chat function
 chatButton.addEventListener('click', function() {
@@ -14,24 +21,50 @@ chatButton.addEventListener('click', function() {
 
 //  Close the chat function
 chatClose.addEventListener('click', function() {
+    /*
+    When windows closes, the options appear again if
+    user wants to chat again
+    */
+    makeOptionsAppear(buttonOptions);    
     chatWindow.style.display = 'none'; // Closes chat window
 });
 
 function chatLinkedinOption (){
-    chatContent.innerHTML = "<p>Has seleccionado Linkedin.</p>";
+    userChatContent.innerHTML = '<p>Sure! Here you have it</p><a href="https://www.linkedin.com/in/miguelangelguaita/" title="My Linkedin">My Linkedin</a>';
+    makeOptionsDisappear(buttonOptions);
 }
 function chatMailOption (){
-    chatContent.innerHTML = "<p>Has seleccionado Mail.</p>";
+    userChatContent.innerHTML = '<p>Okay! Send me an email</p><a href="mailto:miguelanguai@gmail.com?subject=Contact%20From%20Web%20Portfolio&body=Hi%20I%20think%20your%20website%20is%20interesting" title="Send me an email">Mail me</a>';
+    makeOptionsDisappear(buttonOptions);
 }
-function chatcvOption (){
-    chatContent.innerHTML = "<p>Has seleccionado cV.</p>";
+function chatCvOption (){
+    userChatContent.innerHTML = '<p>Great! You can download my CV from this link: <a href="assets/pdf/miguelAngelGuaitaCV.pdf" download="resume-MiguelAngel-Guaita.pdf" title="My updated CV">Download CV (PDF, 164 kB)</a></p>';   
+    makeOptionsDisappear(buttonOptions);
 }
 function chatProjectsOption (){
-    chatContent.innerHTML = "<p>Has seleccionado Projects.</p>";
+    userChatContent.innerHTML = `<p>Sure! Let's guide you to the projects I did in the past</p> <p> <a href="#projects" title="My Projects">My Projects</a></p>`;
+    makeOptionsDisappear(buttonOptions);
+}
+
+function makeOptionsDisappear(buttonOptions){
+    buttonOptions.forEach(element => {
+        element.style.display='none';    
+    });
+    /*As options disappear, even if the window for chat is closed,
+    it's needed to make them appear again if the windows is closed,
+    in order to start chatting again if the user wants to. So, 
+    I need to create a makeOptionsAppear */
+}
+
+function makeOptionsAppear(buttonOptions){
+    buttonOptions.forEach(element => {
+        element.style.display='block';    
+    });
+    userChatContent.innerHTML="<p></p>"
 }
 
 linkedinOption.addEventListener('click', chatLinkedinOption);
 mailOption.addEventListener('click', chatMailOption);
-cvOption.addEventListener('click', chatcvOption);
+cvOption.addEventListener('click', chatCvOption);
 projectsOption.addEventListener('click', chatProjectsOption);
 
